8a11f83181e2a81ea30c2222ab73d6796692d2d1
David Antoon <david@frontegg.com>
2023-10-26 16:47:01 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export * from './lib/harmor';
+import * as fs from 'fs';
+
+
+const filePath = '/Users/davidfrontegg/git/harmor/src/data/test1.har'
+const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+
+
+
+console.log("test1111")
